40e2bda1f0b925c5da6f47410f0a07580fa91057
jean-marie leroux <jmleroux.pro@gmail.com>
2023-02-27 11:09:20 +0100

    NEL-56: Test TranslatedProgressSelector (#63)
    Co-authored-by: Steven VAIDIE <steven.vaidie@gmail.com>

--- src/Akeneo/Crowdin/TranslatedProgressSelector.php
+++ src/Akeneo/Crowdin/TranslatedProgressSelector.php
@@ -9,8 +9,6 @@ use Symfony\Component\EventDispatcher\EventDispatcherInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 
 /**
- * Class TranslatedProgressSelector
- *
  * @author    Pierre Allard <pierre.allard@akeneo.com>
  * @copyright 2016 Akeneo SAS (http://www.akeneo.com)
  * @license   http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
